7a1fa959b0e98aa563402c8b78a4ec23ab818bf1
Unicorn Rentals <elasticvolcano@gmail.com>
2019-01-25 10:47:36 +0000

    yet another quality commit

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = '98e4cdb6ace1f1e95c84c64d1ab908a3'
-__version__ = 'b5e5c26219842ea1c1e665c85e6642eb'
+__vcs_id__ = 'f5e10c94808a676d91f3c592344cb5d3'
+__version__ = '83579c0f8baec79f63f59f8c8ecd27f7'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
